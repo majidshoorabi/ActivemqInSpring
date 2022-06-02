a81c0f411707d486625de6c8deea2282ccbc5ff6
majijdshoorabi <majidshoorabi@gmail.com>
2022-06-02 12:59:20 +0430

    feat - add config for messageListener

--- src/main/java/com/github/majidshoorabi/config/AppConfig.java
+++ src/main/java/com/github/majidshoorabi/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.github.majidshoorabi.config;
 
+import com.github.majidshoorabi.receiver.MessageListener;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.listener.MessageListenerContainer;
 
 import javax.jms.ConnectionFactory;
 
@@ -17,9 +20,20 @@ import javax.jms.ConnectionFactory;
 @ComponentScan("com.github.majidshoorabi")
 public class AppConfig {
 
+    private static final String queueName = "TestQueue";
+
     @Bean
     public ConnectionFactory connectionFactory(){
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
         return connectionFactory;
+    }
+
+    @Bean
+    public MessageListenerContainer messageListenerContainer(){
+        DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setMessageListener(new MessageListener());
+        container.setDestinationName(queueName);
+        return container;
     }
 }
