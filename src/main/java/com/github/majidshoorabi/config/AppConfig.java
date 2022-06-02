ce99829f108255da5ae17119233272513236c6c6
majijdshoorabi <majidshoorabi@gmail.com>
2022-06-02 12:01:26 +0430

    feat - add AppConfig class

---
+++ src/main/java/com/github/majidshoorabi/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.github.majidshoorabi.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.ConnectionFactory;
+
+/**
+ * @author majid.shoorabi
+ * @created 2022-02-June
+ * @project JmsActivemq
+ */
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+        return connectionFactory;
+    }
+}
