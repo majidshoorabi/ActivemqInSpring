74764f2488bb1ec4ab56477e1383f6239a963b0d
majijdshoorabi <majidshoorabi@gmail.com>
2022-06-02 12:03:31 +0430

    feat - add JmsActiveMQApplication.java

---
+++ src/main/java/com/github/majidshoorabi/JmsActiveMQApplication.java
@@ -0,0 +1,31 @@
+package com.github.majidshoorabi;
+
+
+import com.github.majidshoorabi.config.AppConfig;
+import com.github.majidshoorabi.receiver.MessageReceiver;
+import com.github.majidshoorabi.sender.MessageSender;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author majid.shoorabi
+ * @created 2022-02-June
+ * @project IntelliJ IDEA
+ */
+
+public class JmsActiveMQApplication {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String queueName = "TestQueue";
+
+        MessageSender sender = context.getBean(MessageSender.class);
+        sender.sendMessage(queueName,"This is a test message");
+
+
+        MessageReceiver receiver =context.getBean(MessageReceiver.class);
+        receiver.receiveMessage(queueName);
+
+    }
+}
